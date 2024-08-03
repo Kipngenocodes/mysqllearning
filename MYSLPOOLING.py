fd34097f1619cd900a082f76b7296678cc13494b
Kipngenocodes <kipngenoalex.c@gmail.com>
2024-08-02 18:39:04 -0700

    Added a mypooling file

---
+++ MYSLPOOLING.py
@@ -0,0 +1,40 @@
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector.pooling import MySQLConnectionPool
+
+try:
+    # Create a connection pool named 'LittleLemonPool'
+    pool = MySQLConnectionPool(
+        pool_name="LittleLemonPool",
+        pool_size=5,
+        pool_reset_session=True,
+        host='localhost',
+        user='root',
+        password='Kapsabet',
+        database='LittleLemonDB'
+    )
+    print("Connection pool is created successfully.")
+
+    # Get connection from connection pool
+    connection = pool.get_connection()
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL database using connection pool... MySQL Server version on ", db_Info)
+
+        # Create a cursor object using the connection
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to - ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL using Connection pool ", e)
+
+finally:
+    # Closing the connection
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+
